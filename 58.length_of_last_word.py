fcc8b0cb7945a07a1e84e5199a7ec9cd9a3ef16c
Vishvabharti <38591056+Vishavabharti@users.noreply.github.com>
2024-03-25 21:11:36 +0530

    practice

---
+++ 58.length_of_last_word.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.strip()
+        return len(s.split(" ")[-1])
+
+
+# Strip the string to remove empty spaces
+# Split the string will return the list of words
