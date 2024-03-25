c35937bdef9e9082245dc0534d5ee2d1e0d895a2
Vishvabharti <38591056+Vishavabharti@users.noreply.github.com>
2024-03-25 20:39:01 +0530

    practice

--- 242.valid_anagram.py
+++ 242.valid_anagram.py
@@ -30,29 +30,29 @@ class Solution(object):
         return hashmap_s == hashmap_t
 
 
-        # First check the len of both the strings
-        # Create empty hahsmap for both the strings
-        # Check the count of letter in th each strings
-        # If count of char matches then return True else False
+# First check the length of both strings
+# Create an empty hashmap for both the strings
+# Check the count of letters in each string
+# If the count of chars matches then return True else False
 
-        # soem other optimized solutions
-        #-------------------------------------
-        # for char in t:
-        #     if char in hashmap.keys():
-        #         if hashmap[char] == 1:
-        #             del hashmap[char]
-        #         else:
-        #             hashmap[char] -=1
-        #     else:
-        #         return False
+# Some other optimized solutions
+#-------------------------------------
+# for char in t:
+#     if char in hashmap.keys():
+#         if hashmap[char] == 1:
+#             del hashmap[char]
+#         else:
+#             hashmap[char] -=1
+#     else:
+#         return False
 
-        # if len(hashmap) == 0:
-        #     return True
-        # else:
-        #     return False
-        #--------------------------------
-        # for w in set(s):
-        #     if s.count(w)!=t.count(w):
-        #         return False
-        # return True
-        #----------------------------------
+# if len(hashmap) == 0:
+#     return True
+# else:
+#     return False
+#--------------------------------
+# for w in set(s):
+#     if s.count(w)!=t.count(w):
+#         return False
+# return True
+#----------------------------------
